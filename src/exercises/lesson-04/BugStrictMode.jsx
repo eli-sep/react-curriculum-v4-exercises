67452f906a2e9004dbb927c4db44c16c71e062c1
Elias Sepulveda <sepulveda.elias19@gmail.com>
2026-07-28 20:52:12 -0700

    Lesson-04 - StrictMode timer  bug fix.

--- src/exercises/lesson-04/BugStrictMode.jsx
+++ src/exercises/lesson-04/BugStrictMode.jsx
@@ -7,9 +7,13 @@ export default function BugStrictMode() {
   const [count, setCount] = useState(0);
 
   useEffect(() => {
-    setInterval(() => {
+    const timer = setInterval(() => {
       setCount((c) => c + 1);
     }, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
   }, []);
 
   return (
@@ -21,3 +25,5 @@ export default function BugStrictMode() {
 }
 
 // Write your explanation of how StrictMode helps us catch this bug
+// Strict mode mounts, runs effect, unmounts, runs cleanup, mounts again, runs effect again.
+// But because there was no cleanup, Strict mode mounted and ran the code again and it created a second time that would essential added 2 to the count each time.
