f7168b16999d64d3b4883beb0213f42f2431b565
Elias Sepulveda <sepulveda.elias19@gmail.com>
2026-08-16 21:36:18 -0700

    Lesson 6 - Final

--- src/exercises/lesson-06/studentWork.jsx
+++ src/exercises/lesson-06/studentWork.jsx
@@ -1,29 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import UserProfile from './components/UserProfile';
 import TaskFilter from './components/TaskFilter';
 import TaskItem from './components/TaskItem';
 import { handleFilter } from './utils/handleFilter';
+import { useDataloader } from './hooks/useDataLoader';
 
 export default function StudentWork() {
-  const [tasks, setTasks] = useState([]);
   const [filter, setFilter] = useState('all');
-  const [loading, setLoading] = useState(true);
+  const { tasks, loading } = useDataloader();
 
-  //  #1: Data fetching + state + UI logic all mixed together
-  useEffect(() => {
-    const timeout = setTimeout(() => {
-      setTasks([
-        { id: 1, title: 'Learn React', completed: true },
-        { id: 2, title: 'Refactor code', completed: false },
-        { id: 3, title: 'Organize files', completed: false },
-      ]);
-      setLoading(false);
-    }, 500);
-
-    return () => clearTimeout(timeout);
-  }, []);
-
-  // #2: Filtering logic inside component
   const visibleTasks = handleFilter(tasks, filter);
 
   if (loading) {
